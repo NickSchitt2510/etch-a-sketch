a0afa5a2e6097e5402cd7890f9b752c42b496d9b
NickSchitt2510 <121604402+NickSchitt2510@users.noreply.github.com>
2023-07-28 15:54:48 +0800

    Add feature to change the gridSize

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const GRIDSIZE = 16;
-
-// Create div
-function createGrid() {
+// Create div of the grid
+function createGrid(gridSize) {
 
     const container = document.querySelector('.container');
 
-    for (let i = 0; i < GRIDSIZE; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const column = document.createElement('div');
 
         column.classList.add('outline', 'column');
-        for (let j = 0; j < GRIDSIZE; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const cell = document.createElement('div');
             cell.classList.add('outline', 'cell');
             column.appendChild(cell);
@@ -22,12 +20,50 @@ function hoverEffect(className) {
     // Set up eventlistener for all the div
     const cells = document.querySelectorAll('.container > div > div')
     cells.forEach((cell) => {
-        console.log(cell);
         cell.addEventListener('mouseover', () => {
             cell.classList.add(className);
         });
     })
 }
 
-createGrid();
+function removeGrid() {
+    // Remove grid
+    const cells = document.querySelectorAll('.container > div > div');
+    cells.forEach(cell => {
+        cell.remove();
+    });
+    const cols = document.querySelectorAll('.container > div');
+    cols.forEach(col => {
+        col.remove();
+    });
+}
+
+function changeGridSize() {
+    let slider = document.getElementById('gridRange');
+    createGrid(slider.value);
+    slider.onchange = function () {
+        removeGrid();
+        console.log(this.value);
+        createGrid(this.value);
+        hoverEffect('change-black');
+    }
+}
+
+function toggleNoLine() {
+    const cells = document.querySelectorAll('.container > * > *');
+    cells.forEach((cell) => {
+        cell.classList.remove('outline');
+    });
+    const cols = document.querySelectorAll('.container > *');
+    cols.forEach((col) => {
+        col.classList.remove('outline');
+    });
+    const container = document.getElementById('container');
+    console.log(container);
+    container.style.borderTop = "2px solid black";
+    container.style.borderLeft = "2px solid black";
+}
+
+
+changeGridSize();
 hoverEffect('change-black');
